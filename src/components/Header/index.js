fe5c0b338df6e72d128fb32b2be46ac49e60ad99
Xuan19 <xuanwang602@yahoo.com>
2021-04-09 03:13:23 +0200

    settings-menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import logo from 'src/assets/logo.png';
 // import bgImg from 'src/assets/header.jpg';
@@ -13,17 +13,37 @@ const Header = ({ logOut, isLogged }) => {
     logOut();
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+  const showSettings = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
       {/* <img src={logo} alt="logo" to="/" /> */}
       <div className="logo"><Link to="/"><img src={logo} alt="logo" /></Link></div>
       <div className="slogan">Voyager c'est la santé</div>
       <div className="navigation-login-content">
+
         <Link className="navigation-login" to="/favoris"><Icon name="heart outline" />Favoris</Link>
-        {isLogged && (<Link className="navigation-login" to="/Profile">Profile</Link>)}
-        {isLogged && (<Link className="navigation-login" to="/Profile">Mon Compte</Link>)}
+        {isLogged && (
+          <Link onClick={showSettings} className="navigation-login">
+            <Icon name="user circle outline" />
+            Mon Compte
+            {isOpen && (
+              <div className="settings-menu">
+                <div className="navigation-login-menu">
+                  <Link to="/Profile">Profile</Link>
+                </div>
+                <div className="navigation-login-menu">
+                  <Link onClick={handleLogout} to="/">Deconnexion</Link>
+                </div>
+              </div>
+            )}
+          </Link>
+        )}
         {!isLogged && (<Link className="navigation-login" to="/connexion"><Icon name="user circle outline" />Connexion</Link>)}
-        {isLogged && (<Link onClick={handleLogout} className="navigation-login" to="/">Deconnexion</Link>)}
+
       </div>
     </div>
   );
