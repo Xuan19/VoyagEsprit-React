2636bc23fb0c56451b67e49a6f3f45dc4f5951d7
Xuan19 <xuanwang602@yahoo.com>
2022-02-26 11:56:43 +0100

    change BrowserRooter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 // == Import : local
